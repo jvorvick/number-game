0188d96fe2b73e7d514eed40c1d72ef36b689aa3
jvorvick <49791235+jvorvick@users.noreply.github.com>
2021-11-09 18:40:42 -0800

    first commit

---
+++ number_game.py
@@ -0,0 +1,22 @@
+'''
+Number Guessing Game
+
+User will try to guess a number between 1 and 100, chosen by the computer.
+'''
+
+import random
+
+computer = random.randint(1, 100)
+correct = False
+while not correct:
+    player = int(input('Choose a number between 1 and 100: '))
+
+    if player < computer:
+        message = f'Guess was too low. Try again.'
+    elif player > computer:
+        message = f'Guess was too high. Try again.'
+    else:
+        message = f'You guessed the secret!'
+        correct = True
+
+    print(f"computer: {computer}, player: {player}, {message}")
