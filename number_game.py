f88b79fba14ba1215ffd8e5558790ce53f3b8825
jvorvick <49791235+jvorvick@users.noreply.github.com>
2021-11-20 17:23:05 -0800

    updated project

--- number_game.py
+++ number_game.py
@@ -1,7 +1,7 @@
 '''
 Number Guessing Game
 
-User will try to guess a number between 1 and 100, chosen by the computer.
+Either the player or the computer will try to guess a number between 1 and 100, chosen by the opponent.
 '''
 
 import random
@@ -14,30 +14,25 @@ while True:
 
     # player guesses, computer picks
     if guesser == 'Y':
+        
         # get random number from computer
         computer = random.randint(1, 100)
+        
+        # loop until player guesses correctly
         correct = False
-
-    # loop until player guesses correctly
         while not correct:
             player = int(input('Choose a number between 1 and 100: '))
 
             # compare guess to computer and print out appropriate message
             if player < computer:
-                message = f'Guess was too low. Try again.'
+                message = 'Guess was too low. Try again.'
             elif player > computer:
-                message = f'Guess was too high. Try again.'
+                message = 'Guess was too high. Try again.'
             else:
-                message = f'You guessed the secret!'
-                
+                message = 'You guessed the secret!'
                 correct = True
             
-            print(f"computer: {computer}, player: {player}, {message}")
-        
-        play_again = input('Would you like to play again? Enter "y" or "n": ').upper()
-
-        if play_again != 'Y':
-            break
+            print(f"computer: {computer}, player: {player}, {message}") 
 
     # computer guesses, player piccks    
     else:
@@ -45,5 +40,32 @@ while True:
         # get number from player
         player = int(input('Enter a number between 1 and 100: '))
         
+        # set lowest and highest value for computer to guess
+        lowest = 1
+        highest = 100
         
+        # loop until computer guesses correctly
+        correct = False
+        while not correct:
+
+            computer = random.randint(lowest, highest)
+
+            # compare guess to player. if incorrect, adjust range of guess
+            if computer < player:
+                message = 'Guess was too low. Try again.'
+                lowest = computer + 1
+            elif computer > player:
+                message = 'Guess was too high. Try again.'
+                highest = computer - 1
+            else:
+                message = 'You guessed the secret!'
+                correct = True
+
+            print(f'player: {player}, computer: {computer}, {message}')
+            print(f'guess range: {lowest, highest}')
+
+    play_again = input('Would you like to play again? Enter "y" or "n": ').upper()
+
+    if play_again != 'Y':
+        print('Goodbye!')
         break
